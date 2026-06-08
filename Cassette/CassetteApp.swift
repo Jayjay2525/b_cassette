b1e803d34d5103ebca9f57ec99cbec9861b4efdb
Jayjay2525 <loveandbe@naver.com>
2026-06-08 22:21:09 +0900

    Initial Commit

---
+++ Cassette/CassetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CassetteApp.swift
+//  Cassette
+//
+//  Created by Colin on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct CassetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CassetteTests/CassetteTests.swift
@@ -0,0 +1,19 @@
+//
+//  CassetteTests.swift
+//  CassetteTests
+//
+//  Created by Colin on 6/8/26.
+//
+
+import Testing
+@testable import Cassette
+
+struct CassetteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
